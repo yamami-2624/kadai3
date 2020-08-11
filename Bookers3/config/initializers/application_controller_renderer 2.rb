c17ea773b9b07e161fc33e2ade440befb323fa01
山見駿介 <yamamishunsuke@MacBook-Pro-2.local>
2020-08-11 22:40:19 +0900

    saisaiteishutu

--- Bookers3/config/initializers/application_controller_renderer 2.rb
+++ Bookers3/config/initializers/application_controller_renderer 2.rb
@@ -6,5 +6,3 @@
 #     https: false
 #   )
 # end
-
-Refile.secret_key = 'c1e263234e9dd03e5f395948f9a0ef7bd2a4fad56e84593133137bd0ed40e27eb89045d7931e696af3dc8f629544f56f359a5c15cb2a1519cf937e84b9aa42a8'
